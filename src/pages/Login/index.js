33085bdd0dc885678a906e243c31cb39a9bb06a7
cskmacedo <kleber.barreto@msn.com>
2024-01-08 23:07:16 -0300

    Terminada a logica de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './styles.css';
 
+import api from '../../services/api';
 import logo from '../../assets/java.png';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useNavigate();
+
+  async function login(e) {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    try {
+        const response = await api.post('myaccess/v2/authenticate', data);
+
+        localStorage.setItem('email', email);
+        localStorage.setItem('token', response.data.token);
+
+        history('/password');
+    } catch (ex) {
+        if (ex.response && ex.response.data && ex.response.data.status) {
+            alert(`Erro ${ex.response.data.status}, ${ex.response.data.error}`);
+        } else {
+            alert('Algo deu errado! Tente novamente mais tarde.');
+        }
+    }
+  }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logo} alt="Java logo" className="logo-image" />
 
-                <form>
+                <form onSubmit={login}>
                     <h1>MyAccess V2</h1>
-                    <input placeholder="username" />
-                    <input type="password" placeholder="password" />
+                    <input
+                        placeholder="username"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
 
                     <button type="submit">Entrar</button>
                 </form>
